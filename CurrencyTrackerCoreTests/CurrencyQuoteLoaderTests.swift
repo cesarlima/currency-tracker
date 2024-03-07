299105b4ed98d0dcc78dcf35a3b66f73692d5acf
Cesar Lima <cesarcamposdelima@gmail.com>
2024-03-07 10:12:05 -0300

    Guarantee that the `RemoteQuoteLoader.load` makes one request at a time to the httpClient with the provided URL.

--- CurrencyTrackerCoreTests/CurrencyQuoteLoaderTests.swift
+++ CurrencyTrackerCoreTests/CurrencyQuoteLoaderTests.swift
@@ -19,8 +19,8 @@ final class RemoteQuoteLoader {
         self.httpClient = httpClient
     }
     
-    func load() {
-        
+    func load(from url: URL) async throws {
+        _ = try await httpClient.get(from: url)
     }
 }
 
@@ -30,11 +30,30 @@ final class CurrencyQuoteLoaderTests: XCTestCase {
         let client = HttpClientSpy(result: .failure(makeNSError()))
         let sut = RemoteQuoteLoader(httpClient: client)
         
-        
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
+    
+    func test_load_requestsDataFromURL() async throws {
+        let url = anyURL()
+        let client = HttpClientSpy(result: makeSuccessResponse(withStatusCode: 200, data: Data(), url: url))
+        let sut = RemoteQuoteLoader(httpClient: client)
+        
+        try await sut.load(from: url)
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_load_requestsDataFromURLTwice() async throws {
+        let url = anyURL()
+        let client = HttpClientSpy(result: makeSuccessResponse(withStatusCode: 200, data: Data(), url: url))
+        let sut = RemoteQuoteLoader(httpClient: client)
+        
+        try await sut.load(from: url)
+        try await sut.load(from: url)
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
 }
-
 
 final class HttpClientSpy: HttpClient {
     private(set) var requestedURLs: [URL] = []
@@ -59,4 +78,18 @@ final class HttpClientSpy: HttpClient {
 
 func makeNSError() -> NSError {
     NSError(domain: "any erro", code: 0)
+}
+
+func anyURL() -> URL {
+    URL(string: "http://any-url.com")!
+}
+
+func makeSuccessResponse(withStatusCode code: Int, data: Data, url: URL) -> Swift.Result<(Data, HTTPURLResponse), Error> {
+    let response = HTTPURLResponse(
+        url: url,
+        statusCode: code,
+        httpVersion: nil,
+        headerFields: nil)!
+    
+    return .success((data, response))
 }
