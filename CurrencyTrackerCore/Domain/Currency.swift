cc0fc6fe21a2e317bfd02ee6cb594a0c8eff027e
Cesar Lima <cesarcamposdelima@gmail.com>
2024-03-07 09:15:05 -0300

    Create Currency model

---
+++ CurrencyTrackerCore/Domain/Currency.swift
@@ -0,0 +1,16 @@
+//
+//  Currency.swift
+//  CurrencyTrackerCore
+//
+//  Created by MacPro on 07/03/24.
+//
+
+import Foundation
+
+struct Currency {
+    let name: String
+    let code: String
+    let codeIn: String
+    let quote: Double
+    let quoteDate: Date
+}
