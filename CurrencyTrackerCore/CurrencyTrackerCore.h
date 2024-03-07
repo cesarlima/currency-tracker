ea9d82a792b0edd01a05ab235205b0fb675d9785
Cesar Lima <cesarcamposdelima@gmail.com>
2024-03-07 09:04:16 -0300

    Initial Commit

---
+++ CurrencyTrackerCore/CurrencyTrackerCore.h
@@ -0,0 +1,18 @@
+//
+//  CurrencyTrackerCore.h
+//  CurrencyTrackerCore
+//
+//  Created by MacPro on 07/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CurrencyTrackerCore.
+FOUNDATION_EXPORT double CurrencyTrackerCoreVersionNumber;
+
+//! Project version string for CurrencyTrackerCore.
+FOUNDATION_EXPORT const unsigned char CurrencyTrackerCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CurrencyTrackerCore/PublicHeader.h>
+
+
