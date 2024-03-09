def0de61802e56d15c000e9244664c96afbcad36
Cesar Lima <cesarcamposdelima@gmail.com>
2024-03-08 22:42:26 -0300

    Move `CurrencyQuoteStoreStub` to its own file into `Helpers` group

--- CurrencyTrackerCoreTests/CacheCurrencyQuoteTests.swift
+++ CurrencyTrackerCoreTests/CacheCurrencyQuoteTests.swift
@@ -124,32 +124,3 @@ final class CacheCurrencyQuoteTests: XCTestCase {
         return (data, currencies)
     }
 }
-
-private final class CurrencyQuoteStoreStub: CurrencyQuoteStore {
-    
-    enum ReceivedMessage: Equatable {
-        case deleteCachedCurrencyQuote(String)
-    }
-    
-    private var deletionResult: Result<Void, Error>?
-    private var insertionResult: Result<Void, Error>?
-    
-    private(set) var receivedMessages: [ReceivedMessage] = []
-    
-    func delete(with codeIn: String) async throws {
-        receivedMessages.append(.deleteCachedCurrencyQuote(codeIn))
-        try deletionResult?.get()
-    }
-    
-    func completeDeletion(with error: Error) {
-        deletionResult = .failure(error)
-    }
-    
-    func save(quotes: [CurrencyQuote]) async throws {
-        try insertionResult?.get()
-    }
-    
-    func completeInsertion(with error: Error) {
-        insertionResult = .failure(error)
-    }
-}
---
+++ CurrencyTrackerCoreTests/Helpers/CurrencyQuoteStoreStub.swift
@@ -0,0 +1,38 @@
+//
+//  CurrencyQuoteStoreStub.swift
+//  CurrencyTrackerCoreTests
+//
+//  Created by MacPro on 08/03/24.
+//
+
+import Foundation
+@testable import CurrencyTrackerCore
+
+final class CurrencyQuoteStoreStub: CurrencyQuoteStore {
+    
+    enum ReceivedMessage: Equatable {
+        case deleteCachedCurrencyQuote(String)
+    }
+    
+    private var deletionResult: Result<Void, Error>?
+    private var insertionResult: Result<Void, Error>?
+    
+    private(set) var receivedMessages: [ReceivedMessage] = []
+    
+    func delete(with codeIn: String) async throws {
+        receivedMessages.append(.deleteCachedCurrencyQuote(codeIn))
+        try deletionResult?.get()
+    }
+    
+    func completeDeletion(with error: Error) {
+        deletionResult = .failure(error)
+    }
+    
+    func save(quotes: [CurrencyQuote]) async throws {
+        try insertionResult?.get()
+    }
+    
+    func completeInsertion(with error: Error) {
+        insertionResult = .failure(error)
+    }
+}
