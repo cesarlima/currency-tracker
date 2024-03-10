755b72cefa24aedd0ffe5a2a21bb230bc84b40df
Cesar Lima <cesarcamposdelima@gmail.com>
2024-03-10 16:42:38 -0300

    Add `CurrencyTrackerApp` target

---
+++ CurrencyTrackerApp/CurrencyTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyTrackerAppApp.swift
+//  CurrencyTrackerApp
+//
+//  Created by MacPro on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
