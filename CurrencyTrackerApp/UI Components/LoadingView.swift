b64c767d12f8214ebdd2572fa4589d01c8eb43a1
Cesar Lima <cesarcamposdelima@gmail.com>
2024-03-10 22:59:29 -0300

    Add `LoadingView` component

---
+++ CurrencyTrackerApp/UI Components/LoadingView.swift
@@ -0,0 +1,31 @@
+//
+//  LoadingView.swift
+//  CurrencyTrackerApp
+//
+//  Created by MacPro on 10/03/24.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let view = UIActivityIndicatorView(style: .large)
+        view.color = .darkGray
+        view.startAnimating()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+}
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .opacity(0.7)
+                .ignoresSafeArea(.all)
+            
+            ActivityIndicator()
+        }
+    }
+}
